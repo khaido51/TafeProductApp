b162c55fc76425cd579a48b818bda0aae5cbbe35
khai <khaido@gmail.com>
2018-04-09 14:30:24 +0930

    Commit the branch feat_wrap1.1

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
 
                 totalChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25) ;
+                totalWrapTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
             }
             catch (FormatException)
             {
